a3dc0648f27766427a41a09af84493dbcc613773
johncurcio <jfc293@cornell.edu>
2015-10-29 18:53:21 -0400

    Add extract contents from page function

--- src/epub/beautifulSoupTest.py
+++ src/epub/beautifulSoupTest.py
@@ -20,7 +20,7 @@ def remove_comments(soup):
 
 # get the html file as a BeautifulSoup file
 def get_beautiful_file(filename):
-    ex_file = urllib.urlopen(filename).read()
+    ex_file = urllib.urlopen(filename).read().decode('utf-8', 'ignore')
     ex_soup = BeautifulSoup(ex_file)
     return ex_soup
 
@@ -43,18 +43,55 @@ def replace_all_tables(soup):
         for tr in table:
             pass
 
-
-
 def create_epub_compatilble_html(html_filename):
     soup = get_beautiful_file(html_filename)
 
 def remove_non_ascii(text):
     return ''.join(i for i in text if ord(i)<128)
 
+# FUNCTION TO CREATE A HTML FILE GETTING ONLY THE CONTENT FROM THE AUTHOR'S PAGE
+def get_contents_page():
+    print "Type the input HTML file: "
+    html = raw_input()
+    doc = '''<!DOCTYPE html><html lang="en">'''
 
+    if os.path.isfile(html):
+        soup = get_beautiful_file(html)
+        head = soup.head
+        doc += str(head)
+        contents = soup.find("div", {"id": "page-wrapper"})
+        #test if we get contents
+        doc += '''<body>'''
+        doc += str(contents)
+        doc += '''</body></html>'''
+        new_html = BeautifulSoup(doc)
+        pretty_html = new_html.prettify()
+        f = open(r'1_1-epub.html', "w")
+        f.write(pretty_html)
+        f.close()
 
-soup = get_beautiful_file("toc.html")
-for a in soup.findAll("a", href=True):
+def create_toc_file():
+    html = "1_1.html"
+    doc = '''<!DOCTYPE html><html lang="en">'''
+
+    if os.path.isfile(html):
+        soup = get_beautiful_file(html)
+        head = soup.head
+        doc += str(head)
+        contents = soup.find("div", {"role": "navigation"})
+        #test if we get contents
+        doc += '''<body>'''
+        doc += str(contents)
+        doc += '''</body></html>'''
+        new_html = BeautifulSoup(doc)
+        pretty_html = new_html.prettify()
+        f = open(r'toc.html', "w")
+        f.write(pretty_html)
+        f.close()
+
+create_toc_file()
+
+''''for a in soup.findAll("a", href=True):
     pseudo_link = remove_non_ascii(a.text)
     pseudo_link = pseudo_link.strip()
     if os.path.isfile(pseudo_link + '.htm'):
@@ -68,3 +105,4 @@ pretty_html = soup.prettify()
 f = open(r'toc.html', "w")
 f.write(pretty_html)
 f.close()
+'''
