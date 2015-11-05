a8d89d1bb9863579b5ea1b8754a4eb6a106c7a0d
madhavanharish <madhavanharish@gmail.com>
2015-11-05 05:56:32 -0500

    Manually added popup to 2-500.html

--- html/js/controllers.js
+++ html/js/controllers.js
@@ -83,5 +83,52 @@ app.controller('mainCtrl', ['$scope', function($scope) {
 
 app.controller('dynamicExamplesCtrl', ['$scope', function($scope) {
   console.log("In dynamicExamplesCtrl");
+
+  //to enable popups
+   $scope.showModal = false;
+    $scope.buttonClicked = "";
+    $scope.toggleModal = function(btnClicked){
+        $scope.buttonClicked = btnClicked;
+        $scope.showModal = !$scope.showModal;
+    }
 }]);
 
+//Define a modal directive
+app.directive('modal', function () {
+    return {
+      template: '<div class="modal fade">' + 
+          '<div class="modal-dialog">' + 
+            '<div class="modal-content">' + 
+              '<div class="modal-header">' + 
+                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
+              '</div>' + 
+              '<div class="modal-body" ng-transclude></div>' + 
+            '</div>' + 
+          '</div>' + 
+        '</div>',
+      restrict: 'E',
+      transclude: true,
+      replace:true,
+      scope:true,
+      link: function postLink(scope, element, attrs) {
+          scope.$watch(attrs.visible, function(value){
+          if(value == true)
+            $(element).modal('show');
+          else
+            $(element).modal('hide');
+        });
+
+        $(element).on('shown.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = true;
+          });
+        });
+
+        $(element).on('hidden.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = false;
+          });
+        });
+      }
+    };
+  });
